732343c63fe9a4b56ccf2ce3bad8f4423231ad41
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-03-31 00:22:31 -0400

    user authentication check and resizing of app logo

--- DeerDiary/app/src/main/java/com/example/deerdiary/StartActivity.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/StartActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.deerdiary.databinding.ActivityStartBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class StartActivity extends AppCompatActivity {
 
     private ActivityStartBinding activityStartBinding;
+
+    private final FirebaseAuth myAuth = FirebaseAuth.getInstance(); // getting an instance of the auth
+    private FirebaseUser currentUser; // stores current user logged into the app
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +36,16 @@ public class StartActivity extends AppCompatActivity {
             Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        // check for set current user and if no user logged in stay on activity else route to MainActivity
+        currentUser = myAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            startActivity(new Intent(StartActivity.this, MainActivity.class)); // move to main activity
+        }
     }
 }
