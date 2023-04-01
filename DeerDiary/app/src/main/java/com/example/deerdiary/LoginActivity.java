0c62fe9e3581b05ec582817b3b8728da02d29896
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-04-01 17:03:38 -0400

    Issue #2: Login Activity XML and Espresso Test (RED)

--- DeerDiary/app/src/main/java/com/example/deerdiary/LoginActivity.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/LoginActivity.java
@@ -3,12 +3,21 @@ package com.example.deerdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.deerdiary.databinding.ActivityLoginBinding;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding activityLoginBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View v = activityLoginBinding.getRoot();
+        setContentView(v);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
