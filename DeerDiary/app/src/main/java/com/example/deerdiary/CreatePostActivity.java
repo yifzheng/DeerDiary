568dcb8c42c42ce71e30c89c5dbd3d6d8762964a
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2023-04-13 19:11:07 -0400

    added title and storing user inputs in json objects

--- DeerDiary/app/src/main/java/com/example/deerdiary/CreatePostActivity.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/CreatePostActivity.java
@@ -11,6 +11,16 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class CreatePostActivity extends AppCompatActivity {
 
     @Override
@@ -18,15 +28,27 @@ public class CreatePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
 
+        ArrayList<JSONObject> diaryEntries = new ArrayList<JSONObject>();
+
         final Button createButton = findViewById(R.id.createpost_create_button);
         final Button discardButton = findViewById(R.id.createpost_discard_button);
         final TextInputLayout content = findViewById(R.id.createpost_content);
+        final TextInputLayout title = findViewById(R.id.createpost_title);
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                JSONObject newEntry = new JSONObject();
+                try {
+                    newEntry.put("title", title.getEditText().getText());
+                    newEntry.put("content",title.getEditText().getText());
+                    newEntry.put("dateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(new Date()));
+                    diaryEntries.add(newEntry);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(CreatePostActivity.this, StartActivity.class));
-                Toast.makeText(CreatePostActivity.this, content.getEditText().getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(CreatePostActivity.this, newEntry.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -36,6 +58,5 @@ public class CreatePostActivity extends AppCompatActivity {
                 startActivity(new Intent(CreatePostActivity.this, StartActivity.class));
             }
         });
-
     }
 }
