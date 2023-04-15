254f6206a164f7c10421ebd41a2e5a76c3b82283
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2023-04-15 17:12:08 -0400

    Add getter and setter for id

--- DeerDiary/app/src/main/java/com/example/deerdiary/DiaryEntry.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/DiaryEntry.java
@@ -13,6 +13,10 @@ public class DiaryEntry {
         this.dateTime = dateTime;
     }
 
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
     public String getUserId() {
         return userId;
     }
