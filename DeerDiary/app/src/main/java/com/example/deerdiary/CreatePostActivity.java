15748bbd3e492445ac0851363019590144867436
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2023-04-15 19:22:36 -0400

    Refractor to retrieve correct input and content fields

--- DeerDiary/app/src/main/java/com/example/deerdiary/CreatePostActivity.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/CreatePostActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,29 +14,20 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 public class CreatePostActivity extends AppCompatActivity {
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference diaryRef = db.collection("diaryEntry");
-    public TextInputLayout contentLayout;
-    public TextInputLayout titleLayout;
+    public TextInputEditText contentField;
+    public TextInputEditText titleField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +36,8 @@ public class CreatePostActivity extends AppCompatActivity {
 
         final Button createButton = findViewById(R.id.createpost_create_button);
         final Button discardButton = findViewById(R.id.createpost_discard_button);
-        contentLayout = findViewById(R.id.createpost_content);
-        titleLayout = findViewById(R.id.createpost_title);
+        contentField = findViewById(R.id.createpost_content_field);
+        titleField = findViewById(R.id.createpost_title_field);
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,8 +63,8 @@ public class CreatePostActivity extends AppCompatActivity {
 
                 // retrieve current user id from MainActivity
                 userId = MainActivity.currentUserInfo.getString("userId");
-                titleValue = titleLayout.getEditText().getText().toString();
-                contentValue = contentLayout.getEditText().getText().toString();
+                titleValue = titleField.getText().toString();
+                contentValue = contentField.getText().toString();
 
                 // retrieve current system time
                 dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(new Date());
@@ -106,15 +96,13 @@ public class CreatePostActivity extends AppCompatActivity {
     }
 
     public boolean areFieldsPopulated() {
-        if (TextUtils.isEmpty(titleLayout.getEditText().getText())){
-            titleLayout.setError("Title cannot be empty");
-            titleLayout.requestFocus();
-            quickMakeText("Title cannot be empty");
+        if (TextUtils.isEmpty(titleField.getText())){
+            titleField.setError("Title cannot be empty");
+            titleField.requestFocus();
             return false;
-        } else if (TextUtils.isEmpty(contentLayout.getEditText().getText())) {
-            contentLayout.setError("Content cannot be empty");
-            contentLayout.requestFocus();
-            quickMakeText("Content cannot be empty");
+        } else if (TextUtils.isEmpty(contentField.getText())) {
+            contentField.setError("Content cannot be empty");
+            contentField.requestFocus();
             return false;
         } else {
             return true;
