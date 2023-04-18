e22306c0ea49736db2f6b1311bacab9bff1c616c
Collin Shi <cshi0418@gmail.com>
2023-04-18 04:09:00 -0400

    update the diary_view layout and change the query for displaying the entries

--- DeerDiary/app/src/main/java/com/example/deerdiary/MainActivity.java
+++ DeerDiary/app/src/main/java/com/example/deerdiary/MainActivity.java
@@ -8,14 +8,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,8 +34,8 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.core.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,18 +50,14 @@ public class MainActivity extends AppCompatActivity {
     private final CollectionReference diaryRef = db.collection("diaryEntry");
     public static Bundle currentUserInfo = new Bundle(); // relevant user data to be easily accessed in other activities
 
-    RecyclerView recyclerView;
-    ArrayList<DiaryEntry> entries;
-    Adapter adapter;
+    private RecyclerView recyclerView;
+    private ArrayList<DiaryEntry> entries;
+    private Adapter adapter;
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
 
         recyclerView =findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -64,34 +67,32 @@ public class MainActivity extends AppCompatActivity {
         adapter = new Adapter(MainActivity.this, entries);
 
         recyclerView.setAdapter(adapter);
-
         EvenChangeListener();
     }
-
     private void EvenChangeListener() {
-
-        db.collection("diaryEntry")
+        //Retrieve All the documents where the userId field matches the current userId
+        db.collection("diaryEntry").whereEqualTo("userId",currentUserInfo.getString("userId"))
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
 
-                if(error !=null){
-                    Log.e("Firestore error", error.getMessage());
-                    return;
-                }
+                        if(error !=null){
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        }
 
-                for (DocumentChange dc:value.getDocumentChanges()){
+                        for (DocumentChange dc:value.getDocumentChanges()){
 
-                    if(dc.getType() == DocumentChange.Type.ADDED){
+                            if(dc.getType() == DocumentChange.Type.ADDED){
 
-                        entries.add(dc.getDocument().toObject(DiaryEntry.class));
+                                entries.add(dc.getDocument().toObject(DiaryEntry.class));
+                            }
+
+                            adapter.notifyDataSetChanged();
+
+                        }
                     }
-
-                    adapter.notifyDataSetChanged();
-
-                }
-            }
-        });
+                });
     }
 
     @Override
@@ -157,5 +158,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
